92d7c957d57d7a6682b4f8534c0aed4b2148f7d1
vitalii <vitalii.vorobij@gmail.com>
2020-01-09 19:04:28 +0200

    updated settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['afternoon-caverns-37005.herokuapp.com']
 
 
 # Application definition
